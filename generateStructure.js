24d4fad8dd393a09483f6f668381bf25e8829825
kero <kerlossony@yahoo.com>
2024-09-22 15:45:23 +0300

    new one

--- generateStructure.js
+++ generateStructure.js
@@ -2,8 +2,6 @@
 
 const fs = require('fs');
 const path = require('path');
-const { defaultSliceContent } = require('./folderStructure/defaultSliceContent');
-const { defaultComponentContent } = require('./folderStructure/defaultComponentContent');
 
 const folderStructure = [
   {
@@ -32,6 +30,73 @@ const folderStructure = [
     subfolders: []
   },
 ];
+
+const defaultComponentContent = (componentName) => `
+import React from 'react';
+
+export default function ${componentName}(){
+    return (
+      <div>
+        <h1>${componentName}</h1>
+      </div>
+    );
+}
+`;
+
+const defaultSliceContent = (componentName, apiEndpoint) => `
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+export const get${componentName}s = createAsyncThunk(
+  "${componentName.toLowerCase()}/get${componentName}s",
+  async ({ pageSize, currenPage, search }: { pageSize?: number; currenPage?: number; search: string }) => {
+    try {
+      const response = await axios.get(\`${apiEndpoint}?per_page=\${pageSize}&page=\${currenPage}\`, {
+        headers: {
+          handle: search,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+);
+
+export const get${componentName} = createAsyncThunk(
+  "${componentName.toLowerCase()}/get${componentName}",
+  async (id: number | string | undefined) => {
+    try {
+      const response = await axios.get(\`${apiEndpoint}/\${id}\`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+);
+
+const initialState = {
+  ${componentName.toLowerCase()}s: [],
+  ${componentName.toLowerCase()}: {},
+};
+
+const ${componentName}Slice = createSlice({
+  name: '${componentName.toLowerCase()}',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(get${componentName}s.fulfilled, (state, action) => {
+      state.${componentName.toLowerCase()}s = action.payload;
+    }).addCase(get${componentName}.fulfilled, (state, action) => {
+      state.${componentName.toLowerCase()} = action.payload;
+    });
+  },
+});
+
+export default ${componentName}Slice.reducer;
+`;
 
 // Create directories and files recursively
 const createDirectoriesAndFiles = (baseDir, structure, mainFolderName, apiEndpoint) => {
